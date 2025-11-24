56d81d5a8fa71e9578c489db1179b1636ce7cb78
popeye <popeye0618@gmail.com>
2025-11-24 18:14:59 +0900

    initial commit

---
+++ src/main/java/com/recorday/recorday/RecordayApplication.java
@@ -0,0 +1,13 @@
+package com.recorday.recorday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/recorday/recorday/RecordayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recorday.recorday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
