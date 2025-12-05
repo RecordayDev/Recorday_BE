c26e49c674eca9889455f0d4d889d25279c59416
popeye <popeye0618@gmail.com>
2025-12-06 01:03:21 +0900

    refactor: Change `/oauth2/authorize/complete` endpoint from GET to POST in AuthCodeController

--- src/main/java/com/recorday/recorday/auth/oauth2/controller/AuthCodeController.java
+++ src/main/java/com/recorday/recorday/auth/oauth2/controller/AuthCodeController.java
@@ -1,7 +1,7 @@
 package com.recorday.recorday.auth.oauth2.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ public class AuthCodeController {
 
 	private final AuthorizationCodeTokenService authorizationCodeTokenService;
 
-	@GetMapping("/oauth2/authorize/complete")
+	@PostMapping("/oauth2/authorize/complete")
 	public ResponseEntity<Response<TokenResponse>> authCode(@RequestParam String code) {
 		TokenResponse tokenResponse = authorizationCodeTokenService.issueTokens(code);
 
